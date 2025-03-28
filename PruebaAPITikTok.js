0bebbbfbf0607e9a1adbe02fdae3ccbf69274939
idiamer0707 <157150292+idiamer0707@users.noreply.github.com>
2025-03-28 10:45:03 +0100

    Update PruebaAPITikTok.js

--- PruebaAPITikTok.js
+++ PruebaAPITikTok.js
@@ -1,12 +1,12 @@
 const clientKey = 'sbawcifd42tz2khdzw'; 
 const redirectUri = 'https://idiamer0707.github.io/PruebaAPITikTok/'; 
 
-function loginWithTikTok() {
-    const authUrl = `https://www.tiktok.com/auth/authorize?client_key=${clientKey}&redirect_uri=${redirectUri}&response_type=code&scope=user.info.basic`;
-    window.location.href = authUrl;
-}
+document.getElementById('loguin').addEventListener('click', async () => {
 
-async function fetchAccessToken(authCode) {
+  const authUrl = `https://www.tiktok.com/auth/authorize?client_key=${clientKey}&redirect_uri=${redirectUri}&response_type=code&scope=user.info.basic`;
+  window.location.href = authUrl;
+
+  async function fetchAccessToken(authCode) {
     try {
         const response = await fetch('https://www.tiktok.com/auth/token', {
             method: 'POST',
@@ -26,6 +26,7 @@ async function fetchAccessToken(authCode) {
         console.log('Access Token:', data.access_token);
         // Maneja el token aquí (como guardarlo en sesión o usarlo para la API)
     } catch (error) {
-        console.error('Error obteniendo el token:', error);
+      console.error('Error obteniendo el token:', error);
     }
-}
+  }
+});
